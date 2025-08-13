b55edf3b418c7b6c862e0cdfb2e17b4b700befbd
huanchenshang <840943839@qq.com>
2025-08-13 14:38:46 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -4,7 +4,6 @@ PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
-	echo " "
 
 	HP_RULE="surge"
 	HP_PATH="../feeds/luci/applications/luci-app-homeproxy/root/etc/homeproxy"
@@ -28,9 +27,8 @@ fi
 ARGON_FILE="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-argon-config/root/etc/config/argon"
 DIY_FILE="$GITHUB_WORKSPACE/files/etc/config/argon"
 if [ -f "$ARGON_FILE" ]; then
-	echo " "
-
-        cp -f "$DIY_FILE" "$ARGON_FILE"
+ 
+    cp -f "$DIY_FILE" "$ARGON_FILE"
 
 	cd $PKG_PATH && echo "argon主题参数设置成功!"
 fi
@@ -38,7 +36,6 @@ fi
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
 if [ -f "$NSS_DRV" ]; then
-	echo " "
 
 	sed -i 's/START=.*/START=85/g' $NSS_DRV
 
@@ -48,7 +45,6 @@ fi
 #修改qca-nss-pbuf启动顺序
 NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
 if [ -f "$NSS_PBUF" ]; then
-	echo " "
 
 	sed -i 's/START=.*/START=86/g' $NSS_PBUF
 
@@ -58,7 +54,6 @@ fi
 #修复TailScale配置文件冲突
 TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
 if [ -f "$TS_FILE" ]; then
-	echo " "
 
 	sed -i '/\/files/d' $TS_FILE
 
@@ -68,7 +63,6 @@ fi
 #修复Rust编译失败
 RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
 if [ -f "$RUST_FILE" ]; then
-	echo " "
 
 	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
 
@@ -78,7 +72,6 @@ fi
 #修复DiskMan编译失败
 DM_FILE="../package/luci-app-diskman/applications/luci-app-diskman/Makefile"
 if [ -f "$DM_FILE" ]; then
-	echo " "
 
 	sed -i 's/fs-ntfs/fs-ntfs3/g' $DM_FILE
     sed -i '/ntfs-3g-utils /d' $DM_FILE
@@ -92,7 +85,6 @@ MF_FILE="$GITHUB_WORKSPACE/package/v2ray-geodata/Makefile"
 SH_FILE="$GITHUB_WORKSPACE/package/v2ray-geodata/init.sh"
 UP_FILE="$GITHUB_WORKSPACE/package/v2ray-geodata/v2ray-geodata-updater"
 if [ -d "$V2RAY_FILE" ]; then
-	echo " "
 
 	cp -f "$MF_FILE" "$V2RAY_FILE/Makefile"
 	cp -f "$SH_FILE" "$V2RAY_FILE/init.sh"
@@ -106,127 +98,90 @@ wget "https://gist.githubusercontent.com/huanchenshang/df9dc4e13c6b2cd74e0522705
 wget "https://gist.githubusercontent.com/puteulanus/1c180fae6bccd25e57eb6d30b7aa28aa/raw/istore_backend.lua" -O ../package/luci-app-quickstart/luasrc/controller/istore_backend.lua
 
 # 安装opkg distfeeds
-install_opkg_distfeeds() {
-    local emortal_def_dir="$GITHUB_WORKSPACE/wrt/package/emortal/default-settings"
-    local distfeeds_conf="$emortal_def_dir/files/99-distfeeds.conf"
+emortal_def_dir="$GITHUB_WORKSPACE/wrt/package/emortal/default-settings"
+distfeeds_conf="$emortal_def_dir/files/99-distfeeds.conf"
 
-    if [ -d "$emortal_def_dir" ] && [ ! -f "$distfeeds_conf" ]; then
-        cat <<'EOF' >"$distfeeds_conf"
+if [ -d "$emortal_def_dir" ] && [ ! -f "$distfeeds_conf" ]; then
+    cat <<'EOF' >"$distfeeds_conf"
 src/gz openwrt_base https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/base/
 src/gz openwrt_luci https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/luci/
 src/gz openwrt_packages https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/packages/
 src/gz openwrt_routing https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/routing/
 src/gz openwrt_telephony https://downloads.immortalwrt.org/releases/24.10-SNAPSHOT/packages/aarch64_cortex-a53/telephony/
 EOF
-        sed -i "/define Package\/default-settings\/install/a\\
+    sed -i "/define Package\/default-settings\/install/a\\
 \\t\$(INSTALL_DIR) \$(1)/etc\\n\
 \t\$(INSTALL_DATA) ./files/99-distfeeds.conf \$(1)/etc/99-distfeeds.conf\n" $emortal_def_dir/Makefile
 
-        sed -i "/exit 0/i\\
+    sed -i "/exit 0/i\\
 [ -f \'/etc/99-distfeeds.conf\' ] && mv \'/etc/99-distfeeds.conf\' \'/etc/opkg/distfeeds.conf\'\n\
 sed -ri \'/check_signature/s@^[^#]@#&@\' /etc/opkg.conf\n" $emortal_def_dir/files/99-default-settings
-		cd $PKG_PATH && echo "软件源修改成功!"
-    fi
-}
+	cd $PKG_PATH && echo "软件源修改成功!"
+fi
 
 #修改CPU 性能优化调节名称显示
-update_cpufreq_config() {
-    local path="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-cpufreq"
-    local po_file="$path/po/zh_Hans/cpufreq.po"
+cpu_path="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-cpufreq"
+po_file="$cpu_path/po/zh_Hans/cpufreq.po"
 
-    if [ -d "$path" ] && [ -f "$po_file" ]; then
-        sed -i 's/msgstr "CPU 性能优化调节"/msgstr "性能调节"/g' "$po_file"
-        cd $PKG_PATH && echo "cpu调节更名成功"
-    else
-        echo "cpufreq.po文件未找到"
-    fi
-}
+if [ -d "$cpu_path" ] && [ -f "$po_file" ]; then
+    sed -i 's/msgstr "CPU 性能优化调节"/msgstr "性能调节"/g' "$po_file"
+    cd $PKG_PATH && echo "cpu调节更名成功"
+else
+    echo "cpufreq.po文件未找到"
+fi
 
 #修改Argon 主题设置名称显示
-update_argon_config() {
-    local path="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-argon-config"
-    local po_file="$path/po/zh_Hans/argon-config.po"
+argon_path="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-argon-config"
+argonpo_file="$argon_path/po/zh_Hans/argon-config.po"
 
-    if [ -d "$path" ] && [ -f "$po_file" ]; then
-        sed -i 's/msgstr "Argon 主题设置"/msgstr "主题设置"/g' "$po_file"
-        cd $PKG_PATH && echo "主题设置更名成功"
-    else
-        echo "argon-config.po文件没有找到"
-    fi
-}
-
-#turboacc设置名称显示
-update_turboacc_config() {
-    local path="$GITHUB_WORKSPACE/wrt/feeds/ledeluci/applications/luci-app-turboacc"
-    local po_file="$path/po/zh_Hans/turboacc.po"
-
-    if [ -d "$path" ] && [ -f "$po_file" ]; then
-        sed -i 's/msgstr "Turbo ACC 网络加速"/msgstr "网络加速"/g' "$po_file"
-        cd $PKG_PATH && echo "turbocc更名成功"
-    else
-        echo "turboacc.po文件未找到"
-    fi
-}
+if [ -d "$argon_path" ] && [ -f "$argonpo_file" ]; then
+    sed -i 's/msgstr "Argon 主题设置"/msgstr "主题设置"/g' "$argonpo_file"
+    cd $PKG_PATH && echo "主题设置更名成功"
+else
+    echo "argon-config.po文件没有找到"
+fi
 
 #添加quickfile文件管理
-add_quickfile() {
-    local repo_url="https://github.com/sbwml/luci-app-quickfile.git"
-    local target_dir="$GITHUB_WORKSPACE/wrt/package/emortal/quickfile"
-    if [ -d "$target_dir" ]; then
-        rm -rf "$target_dir"
-    fi
-    git clone --depth 1 "$repo_url" "$target_dir"
+quickfile_url="https://github.com/sbwml/luci-app-quickfile.git"
+quickfile_dir="$GITHUB_WORKSPACE/wrt/package/emortal/quickfile"
+if [ -d "$quickfile_dir" ]; then
+    rm -rf "$quickfile_dir"
+fi
+git clone --depth 1 "$quickfile_url" "$quickfile_dir"
 
-    local makefile_path="$target_dir/quickfile/Makefile"
-    if [ -f "$makefile_path" ]; then
-        sed -i '/\t\$(INSTALL_BIN) \$(PKG_BUILD_DIR)\/quickfile-\$(ARCH_PACKAGES)/c\
+makefile_path="$quickfile_dir/quickfile/Makefile"
+if [ -f "$makefile_path" ]; then
+    sed -i '/\t\$(INSTALL_BIN) \$(PKG_BUILD_DIR)\/quickfile-\$(ARCH_PACKAGES)/c\
 \tif [ "\$(ARCH_PACKAGES)" = "x86_64" ]; then \\\
 \t\t\$(INSTALL_BIN) \$(PKG_BUILD_DIR)\/quickfile-x86_64 \$(1)\/usr\/bin\/quickfile; \\\
 \telse \\\
 \t\t\$(INSTALL_BIN) \$(PKG_BUILD_DIR)\/quickfile-aarch64_generic \$(1)\/usr\/bin\/quickfile; \\\
 \tfi' "$makefile_path"
 	cd $PKG_PATH && echo "quickfie添加成功!"
-    fi
-}
+fi
 
 #更换argon源
-update_argon() {
-    local repo_url="https://github.com/huanchenshang/luci-theme-argon.git"
-    local dst_theme_path="../feeds/luci/themes/luci-theme-argon"
-    local tmp_dir=$(mktemp -d)
+argon_url="https://github.com/huanchenshang/luci-theme-argon.git"
+dst_theme_path="../feeds/luci/themes/luci-theme-argon"
+tmp_dir=$(mktemp -d)
 
-    echo "正在更新 argon 主题..."
-
-    git clone --depth 1 "$repo_url" "$tmp_dir"
+    git clone --depth 1 "$qrgon_url" "$tmp_dir"
 
     rm -rf "$dst_theme_path"
     rm -rf "$tmp_dir/.git"
     mv "$tmp_dir" "$dst_theme_path"
 
     cd $PKG_PATH && echo "luci-theme-argon 更新完成"
-}
 
 #修改argon背景图片
-update_argon_background() {
-    local theme_path="$GITHUB_WORKSPACE/wrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background"
-    local source_path="$GITHUB_WORKSPACE/images"
-    local source_file="$source_path/bg1.jpg"
-    local target_file="$theme_path/bg1.jpg"
+theme_path="$GITHUB_WORKSPACE/wrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background"
+source_path="$GITHUB_WORKSPACE/images"
+source_file="$source_path/bg1.jpg"
+target_file="$theme_path/bg1.jpg"
 
-    if [ -f "$source_file" ]; then
-        cp -f "$source_file" "$target_file"
-        cd $PKG_PATH && echo "背景图片更新成功：$target_file"
-    else
-        echo "错误：未找到源图片文件：$source_file"
-        return 1
-    fi
-}
-
-install_opkg_distfeeds
-add_quickfile
-update_argon_config
-update_argon
-#update_turboacc_config
-#update_argon_background
-#update_cpufreq_config
-
+if [ -f "$source_file" ]; then
+    cp -f "$source_file" "$target_file"
+    cd $PKG_PATH && echo "背景图片更新成功：$target_file"
+else
+    echo "错误：未找到源图片文件：$source_file"
+fi
