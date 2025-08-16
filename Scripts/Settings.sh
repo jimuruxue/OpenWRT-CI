74daf5623382c58e378ba99372328190c2a0baa9
huanchenshang <840943839@qq.com>
2025-08-16 17:07:16 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -24,7 +24,7 @@ elif [ -f "$WIFI_UC" ]; then
 	#修改WIFI加密
 	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" $WIFI_UC
  	#开启MU-MIMO和设定功率20
-    sed -i "/set \${s}.disabled='0'/a \
+    sed -i "/set \${s}.disabled='0'/a \n\
             set \${s}.mu_beamformer='1'\n\
             set \${s}.txpower='20'" $WIFI_UC
 fi
