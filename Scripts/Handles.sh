84be81315e99488ec5116dc9210eca0d04d0a2aa
huanchenshang <840943839@qq.com>
2025-08-17 16:59:14 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,27 +2,6 @@
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-
-	HP_RULE="surge"
-	HP_PATH="../feeds/luci/applications/luci-app-homeproxy/root/etc/homeproxy"
-
-	rm -rf ./$HP_PATH/resources/*
-
-	git clone -q --depth=1 --single-branch --branch "release" "https://github.com/Loyalsoldier/surge-rules.git" ./$HP_RULE/
-	cd ./$HP_RULE/ && RES_VER=$(git log -1 --pretty=format:'%s' | grep -o "[0-9]*")
-
-	echo $RES_VER | tee china_ip4.ver china_ip6.ver china_list.ver gfw_list.ver
-	awk -F, '/^IP-CIDR,/{print $2 > "china_ip4.txt"} /^IP-CIDR6,/{print $2 > "china_ip6.txt"}' cncidr.txt
-	sed 's/^\.//g' direct.txt > china_list.txt ; sed 's/^\.//g' gfw.txt > gfw_list.txt
-	mv -f ./{china_*,gfw_list}.{ver,txt} ../$HP_PATH/resources/
-
-	cd .. && rm -rf ./$HP_RULE/
-
-	cd $PKG_PATH && echo "homeproxy数据更新成功"
-fi
-
 #修改argon主题设置
 ARGON_FILE="$GITHUB_WORKSPACE/wrt/feeds/luci/applications/luci-app-argon-config/root/etc/config/argon"
 DIY_FILE="$GITHUB_WORKSPACE/files/etc/config/argon"
@@ -185,9 +164,9 @@ target_file="$theme_path/bg1.jpg"
 
 if [ -f "$source_file" ]; then
     cp -f "$source_file" "$target_file"
-    cd $PKG_PATH && echo "背景图片更新成功：$target_file"
+    cd $PKG_PATH && echo "背景图片更新成功"
 else
-    echo "错误：未找到源图片文件：$source_file"
+    echo "错误：未找到源图片文件"
 fi
 
 #添加定时清理内存
