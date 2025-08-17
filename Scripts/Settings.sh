fba5e57eb882d81ec019cd69a1d82317a9c76472
huanchenshang <840943839@qq.com>
2025-08-17 17:28:55 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -9,24 +9,23 @@ sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find .
 
 WIFI_SH=$(find ./target/linux/{mediatek/filogic,qualcommax}/base-files/etc/uci-defaults/ -type f -name "*set-wireless.sh" 2>/dev/null)
 WIFI_UC="./package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc"
+WIFI_CU="$GITHUB_WORKSPACE/files/mac80211.uc"
 if [ -f "$WIFI_SH" ]; then
 	#修改WIFI名称
 	sed -i "s/BASE_SSID='.*'/BASE_SSID='$WRT_SSID'/g" $WIFI_SH
 	#修改WIFI密码
 	sed -i "s/BASE_WORD='.*'/BASE_WORD='$WRT_WORD'/g" $WIFI_SH
-elif [ -f "$WIFI_UC" ]; then
+elif [ -f "$WIFI_CU" ] && [ -f "$WIFI_UC" ]; then
+	#替换修改的WIFI配置文件
+ 	cp -f "$WIFI_CU" "$WIFI_UC"
 	#修改WIFI名称
-	sed -i "s/ssid='.*'/ssid='$WRT_SSID'/g" $WIFI_UC
+	sed -i "s/ImmortalWrt/'$WRT_SSID'/g" $WIFI_UC
 	#修改WIFI密码
 	sed -i "s/key='.*'/key='$WRT_WORD'/g" $WIFI_UC
 	#修改WIFI地区
 	sed -i "s/country='.*'/country='CN'/g" $WIFI_UC
 	#修改WIFI加密
 	sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" $WIFI_UC
- 	#开启MU-MIMO和设定功率20
-    sed -i "/set \${s}.disabled='0'/a \
-            set \${s}.mu_beamformer='1'\
-            set \${s}.txpower='20'" $WIFI_UC
 fi
 
 CFG_FILE="./package/base-files/files/bin/config_generate"
