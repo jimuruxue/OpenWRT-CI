e7eec66f00051033774e5078317b1bc8e9e2d8e7
huanchenshang <840943839@qq.com>
2025-08-18 10:28:12 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -192,11 +192,11 @@ else
 fi
 
 #设置使用bbr加速
-BBR_CONF_PATH="$PKG_PATH/base-files/files/etc/sysctl.d/10-bbr.conf"
-if [ ! -f "$BBR_CONF_PATH" ]; then
-    cat <<'EOF' >"$BBR_CONF_PATH"
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-EOF
-    echo "BBR 配置文件创建成功！"
-fi
+#BBR_CONF_PATH="$PKG_PATH/base-files/files/etc/sysctl.d/10-bbr.conf"
+#if [ ! -f "$BBR_CONF_PATH" ]; then
+    #cat <<'EOF' >"$BBR_CONF_PATH"
+#net.core.default_qdisc=fq
+#net.ipv4.tcp_congestion_control=bbr
+#EOF
+    #echo "BBR 配置文件创建成功！"
+#fi
