b250c0eab7c51d1e8cec5992e54e04effb2279c8
huanchenshang <840943839@qq.com>
2025-08-27 22:22:24 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -202,6 +202,16 @@ else
     echo "添加定时清理内存出错"
 fi
 
+#设置使用bbr加速
+#BBR_CONF_PATH="$PKG_PATH/base-files/files/etc/sysctl.d/10-bbr.conf"
+#if [ ! -f "$BBR_CONF_PATH" ]; then
+    #cat <<'EOF' >"$BBR_CONF_PATH"
+#net.core.default_qdisc=fq
+#net.ipv4.tcp_congestion_control=bbr
+#EOF
+    #echo "BBR 配置文件创建成功！"
+#fi
+
 # 替换sing-box版本为reF1nd
 #SINGBOX_MAKEFILE_PATH="$GITHUB_WORKSPACE/wrt/feeds/packages/net/sing-box/Makefile"
 #CUSTOM_SINGBOX_PATH="$GITHUB_WORKSPACE/files/sing-box"
