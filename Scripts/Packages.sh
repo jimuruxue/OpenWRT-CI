642c982a721fe28356017e7efefebe0ff06bf8ed
jimuruxue <1611575262@qq.com>
2025-11-17 19:01:06 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -58,14 +58,8 @@ UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 #UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 #UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
 
-# quickstart
-UPDATE_PACKAGE "taskd" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "luci-lib-xterm" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "luci-lib-taskd" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-store" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "quickstart" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-quickstart" "kenzok8/small-package" "main" "pkg"
-UPDATE_PACKAGE "luci-app-istorex" "kenzok8/small-package" "main" "pkg"
+# istore
+UPDATE_PACKAGE "luci-app-istore" "linkease/istore" "main"
 
 # unishare
 UPDATE_PACKAGE "webdav2" "kenzok8/small-package" "main" "pkg"
