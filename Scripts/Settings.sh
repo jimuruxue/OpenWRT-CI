e40f796c465fde2541d9406329f60c547a70fd9e
jimuruxue <1611575262@qq.com>
2026-05-06 22:02:29 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -42,27 +42,33 @@ echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
+#引入私有扩展配置
+if [ -f "$GITHUB_WORKSPACE/Config/PRIVATE.txt" ]; then
+	echo "Applying private configurations from PRIVATE.txt..."
+	cat $GITHUB_WORKSPACE/Config/PRIVATE.txt >> ./.config
+fi
+
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo -e "$WRT_PACKAGE" >> ./.config
 fi
 
+#无WIFI配置标志
+if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
+	echo "WRT_WIFI=wifi-no" >> $GITHUB_ENV
+fi
+
 #高通平台调整
-DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+DTS_PATH="./target/linux/qualcommax/dts/"
 if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-	#开启sqm-nss插件
-	#echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
-	#echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-	if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
-	else
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
-	fi
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
+	#其他调整
+	echo "CONFIG_PACKAGE_kmod-usb-serial-qualcomm=y" >> ./.config
+
 	#无WIFI配置调整Q6大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
@@ -75,66 +81,7 @@ dhcp_file="./package/network/services/dnsmasq/files/dhcp.conf"
 if [ -f "$dhcp_file" ]; then
     sed -i "s/option dns_redirect\t1/option dns_redirect\t0/g" "$dhcp_file"
 fi
-	
-#以下为添加dae支持
-#conde_file="./target/linux/qualcommax/ipq60xx/config-default"
-#if [ -f "$conde_file" ]; then
-#    cat >> "$conde_file" <<EOF
-#CONFIG_BPF=y
-#CONFIG_BPF_SYSCALL=y
-#CONFIG_BPF_JIT=y
-#CONFIG_CGROUPS=y
-#CONFIG_KPROBES=y
-#CONFIG_NET_INGRESS=y
-#CONFIG_NET_EGRESS=y
-#CONFIG_NET_SCH_INGRESS=m
-#CONFIG_NET_CLS_BPF=m
-#CONFIG_NET_CLS_ACT=y
-#CONFIG_BPF_STREAM_PARSER=y
-#CONFIG_DEBUG_INFO=y
-# CONFIG_DEBUG_INFO_REDUCED is not set
-#CONFIG_DEBUG_INFO_BTF=y
-#CONFIG_KPROBE_EVENTS=y
-#CONFIG_BPF_EVENTS=y
 
-#CONFIG_SCHED_CLASS_EXT=y
-#CONFIG_PROBE_EVENTS_BTF_ARGS=y
-#CONFIG_IMX_SCMI_MISC_DRV=y
-#CONFIG_ARM64_CONTPTE=y
-#CONFIG_TRANSPARENT_HUGEPAGE=y
-#CONFIG_TRANSPARENT_HUGEPAGE_ALWAYS=y
-## CONFIG_TRANSPARENT_HUGEPAGE_MADVISE is not set
-## CONFIG_TRANSPARENT_HUGEPAGE_NEVER is not set
-#EOF
-#    echo "cat_kernel_config to "$conde_file" done"
-#fi
-
-#config_file="./.config"
-#if [ -f "$config_file" ]; then
-#    cat >> $config_file <<EOF
-#CONFIG_DEVEL=y
-#CONFIG_KERNEL_DEBUG_INFO=y
-#CONFIG_KERNEL_DEBUG_INFO_REDUCED=n
-#CONFIG_KERNEL_DEBUG_INFO_BTF=y
-#CONFIG_KERNEL_CGROUPS=y
-#CONFIG_KERNEL_CGROUP_BPF=y
-#CONFIG_KERNEL_BPF_EVENTS=y
-#CONFIG_BPF_TOOLCHAIN_HOST=y
-#CONFIG_KERNEL_XDP_SOCKETS=y
-#CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
-#EOF
-#    echo "cat_ebpf_config to  $config_file done"
-#fi
-
-# 修改内核大小
-#image_file="./target/linux/qualcommax/image/ipq60xx.mk"
-#if [ -f "$image_file" ]; then
-#    sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" "$image_file"
-#    sed -i "/^define Device\/jdcloud_re-cs-02/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#    sed -i "/^define Device\/jdcloud_re-cs-07/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#    sed -i "/^define Device\/redmi_ax5-jdcloud/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#    sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
-#    echo "Kernel size updated in $image_file"
-#else
-#    echo "Image file $image_file not found, skipping kernel size update"
-#fi
+#亚瑟修复USB2.0日志报错问题
+wget -qO - https://github.com/davidtall/immortalwrt/commit/ce39feb4.patch | patch -p1
+cat ./target/linux/qualcommax/dts/ipq6000-re-ss-01.dts
